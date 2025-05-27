fc4c18794b5393536f3c839a0368cb70828dad49
amar6677 <amar6677@gmail.com>
2025-05-27 19:21:50 +0530

    Changes on 27th May Evening

--- LangchainOpenAI.py
+++ LangchainOpenAI.py
@@ -1,43 +1,32 @@
-import requests
+#importing the required packages
 import pandas as pd
-from langchain.chat_models import ChatOpenAI
+#from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+
 import os
-
 # Environment setup
-os.environ["OPENAI_API_KEY"] = "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"
-
+load_dotenv()
+key=os.getenv("OpenAIKey")
+print(f"printing the open AI key \n {key}")
 # Step 1: Fetch Tickets 
-
+#reading the incident CSV file
 # Read the incident CSV
-df = pd.read_csv("")
+df = pd.read_csv("C:\LangChainPractice\langchain-python-practice\sample_incidents.csv")
+print(df.head())
 
-# Optionally filter by group
-your_group = "SupportGroup-XYZ"
-df = df[df["assigned_group"] == your_group]
+#connecting witht he open AI
 
 # Step 2: Setup LangChain LLM
+os.environ["OPENAI_API_KEY"]=key
 llm = ChatOpenAI(model="gpt-4")
-
-prompt = ChatPromptTemplate.from_template("""
-You are a system analyst. A ticket description is given below. Based on it, determine:
-1. Whether it is related to user access.
-2. What specific request is received categorize in 4  1. User New Access 2.Existing User Unable to Access 3.User Needs Revoke the Access 4.Not User Issue.
-
-Description: {description}
-
+prompt = ChatPromptTemplate.from_template(""" Provide me latest news on Narendra Modi
 
 """)
 
 chain = LLMChain(llm=llm, prompt=prompt)
-
-# Step 3: Process Tickets
-actions = []
-for _, row in df.iterrows():
-    result = chain.run(description=row['description'])
-    result_dict = eval(result)  # Or use json.loads(result) if JSON
-    print(result_dict)
-    
-    
-
+print("printing the promt result \n")
+print(chain)
